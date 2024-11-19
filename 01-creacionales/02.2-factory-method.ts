0aaa303504822c70e144a115d9ab7c511b1de97b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-19 15:27:31 -0500

    Implement Factory Method pattern for Hamburger and Report generation with corresponding classes

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -35,19 +35,21 @@ interface Report {
 // Implementar SalesReport e InventoryReport
 
 class SalesReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log('%cGenerando reporte de ventas...', COLORS.green);
+  }
 }
 
 class InventoryReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log('%cGenerando reporte de inventario...', COLORS.orange);
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -59,13 +61,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new InventoryReport();
   }
 }
 
@@ -74,10 +76,7 @@ class InventoryReportFactory extends ReportFactory {
 function main() {
   let reportFactory: ReportFactory;
 
-  const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
-  );
+  const reportType = prompt('¿Qué tipo de reporte deseas? (sales/inventory)');
 
   if (reportType === 'sales') {
     reportFactory = new SalesReportFactory();
