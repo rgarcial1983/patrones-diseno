5b357cf5ce08a4dd15a083ff7c22abe2605efaf0
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-26 11:37:59 -0500

    Implement iterator using Symbol.iterator and generator functions in CardCollection

--- 03-comportamiento/03.3-iterator.ts
+++ 03-comportamiento/03.3-iterator.ts
@@ -29,10 +29,19 @@ class CardCollection {
   }
 
   //TODO: Implementación del iterador usando Symbol.iterator
-  // Symbol.iterator (): IterableIterator<Card>
+  *[Symbol.iterator](): IterableIterator<Card> {
+    yield* this.cards;
+    // for( const card of this.cards ) {
+    //   yield card;
+    // }
+  }
 
   // TODO: Implementación del iterador usando Generadores
-  // *getCard(): IterableIterator<Card>
+  *getCard(): IterableIterator<Card> {
+    for (const card of this.cards) {
+      yield card;
+    }
+  }
 }
 
 // Código Cliente para probar el iterador
