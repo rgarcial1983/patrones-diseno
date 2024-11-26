27bb97d3bec1ab069de35063bb616fe378dbe462
Fernando Herrera <fernando.herrera85@gmail.com>
2024-11-26 13:16:45 -0500

    Implement Mediator pattern methods in ControlTower for airplane registration and messaging

--- 03-comportamiento/04.2-mediator.ts
+++ 03-comportamiento/04.2-mediator.ts
@@ -36,12 +36,16 @@ class ControlTower {
   private airplanes: Airplane[] = [];
 
   // Registrar un avión en la torre de control
-  // TODO: Implementar el método registerAirplane
-  // registerAirplane(airplane: Airplane)
+  registerAirplane(airplane: Airplane) {
+    this.airplanes.push(airplane);
+  }
 
   // Enviar un mensaje de un avión a todos los demás
-  //TODO: Implementar el método sendMessage
-  // sendMessage(sender: Airplane, message: string): void
+  sendMessage(sender: Airplane, message: string): void {
+    this.airplanes
+      .filter((airplane) => airplane !== sender)
+      .forEach((airplane) => airplane.receiveMessage(sender, message));
+  }
 
   // Coordinación de aterrizaje
   requestLanding(sender: Airplane): void {
@@ -75,7 +79,7 @@ class Airplane {
     this.id = id;
     this.controlTower = controlTower;
 
-    // TODO: Registrar el avión en la torre de control
+    controlTower.registerAirplane(this);
   }
 
   getId(): string {
@@ -85,15 +89,14 @@ class Airplane {
   // Solicitar aterrizaje a la torre de control
   requestLanding(): void {
     console.log(`${this.id} solicita permiso para aterrizar.`);
-
-    // TODO: Solicitar aterrizaje a la torre de control
+    this.controlTower.requestLanding(this);
   }
 
   // Solicitar despegue a la torre de control
   requestTakeoff(): void {
     console.log(`${this.id} solicita permiso para despegar.`);
 
-    // TODO: Solicitar despegue a la torre de control
+    this.controlTower.requestTakeoff(this);
   }
 
   // Recibir mensaje de otros aviones
